0a990ddf53eca1dc11d938d225c53bb0f828b298
liangTianyu <826224862@qq.com>
2019-10-17 16:26:03 +0800

    测试

--- src/test/java/com/lty/redis/RedisApplicationTests.java
+++ src/test/java/com/lty/redis/RedisApplicationTests.java
@@ -19,6 +19,21 @@ class RedisApplicationTests {
         Jedis jedis = new Jedis("192.168.11.205",6379);
         jedis.connect();
         System.out.println("jedis.ping():" +jedis.ping());
+   //String
+
+        jedis.set("k1","v1");
+        jedis.set("k2","v2");
+        jedis.set("k3","v3");
+        System.out.println(jedis.get("k3"));
+        jedis.append("k1","append this value");
+        jedis.select(1);
+        jedis.set("k1","0");
+        jedis.incr("k1");
+        System.out.println("加后的结果为："+jedis.get("k1"));
+
+
+
+
     }
 
 }
--- src/test/java/com/lty/redis/TestTx.java
+++ src/test/java/com/lty/redis/TestTx.java
@@ -1,4 +1,37 @@
 package com.lty.redis;
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
 public class TestTx {
+    public boolean trancationMethod() throws Exception{
+        Jedis jedis = new Jedis("192.168.11.205",6379);
+        //开启观测
+        jedis.watch("balance");
+        int consume = 10;
+        Thread.sleep(10000);
+        if( consume>Integer.parseInt(jedis.get("balance") )      ){
+            jedis.unwatch();
+            System.out.println("余额不足！！");
+            return false;
+        }else{
+            //开启事物
+            Transaction transaction = jedis.multi();
+            transaction.decrBy("balance",consume);
+            transaction.incrBy("debt",consume);
+            transaction.exec();
+            System.out.println("当前可用金额  需要还款");
+            System.out.println(jedis.get("balance")+"          "+jedis.get("debt"));
+            return true;
+        }
+
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        TestTx tx = new TestTx();
+        Boolean flag = tx.trancationMethod();
+        System.out.println("此次操作：" +  (flag ? true :"失败"));
+
+    }
 }
